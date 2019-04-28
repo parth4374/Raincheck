851f4f07b95fad0205a5fe4722bacb116abd4d80
hallauren96 <48655308+hallauren96@users.noreply.github.com>
2019-04-28 13:34:05 -0400

    Add files via upload

---
+++ src/main/java/com/example/settings/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.settings;
+
+import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // back button pressed
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
